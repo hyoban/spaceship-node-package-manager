a7fad428b42715b330e855594db561b67deaad90
Stephen Zhou <hi@hyoban.cc>
2024-07-12 02:02:46 +0800

    feat: mvp

--- spaceship-section.plugin.zsh
+++ spaceship-section.plugin.zsh
@@ -1,55 +1,42 @@
-#
-# Foobar
-#
-# Foobar is a supa-dupa cool tool for making you development easier.
-# Link: https://www.foobar.xyz
+SPACESHIP_NODE_PACKAGE_MANAGER_SHOW="${SPACESHIP_NODE_PACKAGE_MANAGER_SHOW=true}"
+SPACESHIP_NODE_PACKAGE_MANAGER_ASYNC="${SPACESHIP_NODE_PACKAGE_MANAGER_ASYNC=true}"
 
-# ------------------------------------------------------------------------------
-# Configuration
-# ------------------------------------------------------------------------------
+spaceship::find_lockfile() {
+  local root="$PWD"
+  local lockfile="$1"
 
-SPACESHIP_FOOBAR_SHOW="${SPACESHIP_FOOBAR_SHOW=true}"
-SPACESHIP_FOOBAR_ASYNC="${SPACESHIP_FOOBAR_ASYNC=true}"
-SPACESHIP_FOOBAR_PREFIX="${SPACESHIP_FOOBAR_PREFIX="$SPACESHIP_PROMPT_DEFAULT_PREFIX"}"
-SPACESHIP_FOOBAR_SUFFIX="${SPACESHIP_FOOBAR_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"}"
-SPACESHIP_FOOBAR_SYMBOL="${SPACESHIP_FOOBAR_SYMBOL="😍 "}"
-SPACESHIP_FOOBAR_COLOR="${SPACESHIP_FOOBAR_COLOR="yellow"}"
+  while [ "$root" ] &&  \
+        [ ! -f "$root/$lockfile" ]; do
+    root="${root%/*}"
+  done
 
-# ------------------------------------------------------------------------------
-# Section
-# ------------------------------------------------------------------------------
+  print "$root"
+}
 
-# Show foobar status
-# spaceship_ prefix before section's name is required!
-# Otherwise this section won't be loaded.
-spaceship_foobar() {
-  # If SPACESHIP_FOOBAR_SHOW is false, don't show foobar section
-  [[ $SPACESHIP_FOOBAR_SHOW == false ]] && return
+spaceship_node_package_manager() {
+  [[ $SPACESHIP_NODE_PACKAGE_MANAGER_SHOW == false ]] && return
 
-  # Check if foobar command is available for execution
-  spaceship::exists foobar || return
+  local package_managers=("pnpm" "yarn" "npm")
+  local lock_files=("pnpm-lock.yaml" "yarn.lock" "package-lock.json")
+  local colors=("yellow" "blue" "red")
 
-  # Show foobar section only when there are foobar-specific files in current
-  # working directory.
+  for i in 1 2 3
+  do
+    local package_manager="${package_managers[$i]}"
+    local lock_file="${lock_files[$i]}"
+    local color="${colors[$i]}"
 
-  # spaceship::upsearch utility helps finding files up in the directory tree.
-  local is_foobar_context="$(spaceship::upsearch foobar.conf)"
-  # Here glob qualifiers are used to check if files with specific extension are
-  # present in directory. Read more about them here:
-  # http://zsh.sourceforge.net/Doc/Release/Expansion.html
-  [[ -n "$is_foobar_context" || -n *.foo(#qN^/) || -n *.bar(#qN^/) ]] || return
-
-  local foobar_version="$(foobar --version)"
-
-  # Check if tool version is correct
-  [[ $foobar_version == "system" ]] && return
-
-  # Display foobar section
-  # spaceship::section utility composes sections. Flags are optional
-  spaceship::section::v4 \
-    --color "$SPACESHIP_FOOBAR_COLOR" \
-    --prefix "$SPACESHIP_FOOBAR_PREFIX" \
-    --suffix "$SPACESHIP_FOOBAR_SUFFIX" \
-    --symbol "$SPACESHIP_FOOBAR_SYMBOL" \
-    "$foobar_version"
+    if spaceship::exists $package_manager; then
+      local is_using=$(spaceship::find_lockfile $lock_file)
+      if [[ -n "$is_using" ]]; then
+        local version="$($package_manager --version)"
+        spaceship::section::v4 \
+          --suffix " " \
+          --symbol "$package_manager " \
+          --color "$color" \
+          "$version"
+        return
+      fi
+    fi
+  done
 }
